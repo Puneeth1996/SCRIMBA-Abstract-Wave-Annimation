02f98cb8d55e1e3a22a4fc0e967f82d5fada38f0
Puneeth1996 <puneeth1996p@gmail.com>
2018-09-17 12:09:49 +0530

    initial project version

---
+++ index.php
@@ -0,0 +1,21 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="index.css">
+    </head>
+    <body>
+        <div class="container">
+            <span></span>
+            <span></span>
+            <span></span>
+			
+            <span></span>
+            <span></span>
+            <span></span>
+			
+            <span></span>
+            <span></span>
+            <span></span>
+			
+        </div>
+    </body>
+</html>
